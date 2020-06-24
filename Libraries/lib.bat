679c918439b4c438a4f68188f0f50f81bc5bd6ce
0f3rn <oscar.a.fernandez@sogeti.com>
2020-06-24 22:12:05 +0200

    done

--- Libraries/lib.bat
+++ Libraries/lib.bat
@@ -1,7 +1,2 @@
-pip install jsonpath
-pip install openpyxl 
 pip install robotframework
 pip install robotframework-SeleniumLibrary
-pip install robotframework-SikuliLibrary
-pip install robotframework-requests
-pip install robotframework-jsonlibrary
