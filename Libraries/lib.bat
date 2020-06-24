be7851ac7e6114ed66578f94b2613d209196dc29
0f3rn <oscar.a.fernandez@sogeti.com>
2020-06-24 15:34:14 +0200

    Added Lib.bat and Drivers

---
+++ Libraries/lib.bat
@@ -0,0 +1,7 @@
+pip install jsonpath
+pip install openpyxl 
+pip install robotframework
+pip install robotframework-SeleniumLibrary
+pip install robotframework-SikuliLibrary
+pip install robotframework-requests
+pip install robotframework-jsonlibrary
